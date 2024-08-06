8397e1014155af64519bd47d90487b32d7878985
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-06 21:49:10 +0300

    Adds the update section

--- bank/src/components/Transactions.jsx
+++ bank/src/components/Transactions.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Transactions = () => {
     // Since code is dynamic I'll use state hooks
     const[transaction, setTransaction] = useState({})
 
     //Remember to add a params
+    //Use params returns object containing the routes dynamic parameters
+    {/*const {categories} = useParams()*/}
+
 
     // Since code needs data from a server I'll have to use the useEffect hook
-    useEffect(() => {
+
+    
+    {/*useEffect(() => {
         // Update the fetch method to be the template
         fetch(`http://localhost:3000/transactions`)
         .then(res => res.json)
         .then(transaction => setTransaction(transaction))
         .catch(error => console.log(error)
         )
-    },[categories])
+    },[categories])*/}
 
   return (
     // Information from above will be passed down here
