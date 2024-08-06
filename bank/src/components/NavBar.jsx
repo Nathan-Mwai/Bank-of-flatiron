40fb9aa0ea978703102662d4c8c5e71eecae8b31
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-06 19:05:41 +0300

    Adds server and components

---
+++ bank/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <>
+        {/* <NavLink to="/">Home</NavLink> */}
+      </>
+      <>
+      {/* <NavLink to="/transaction">Make Transaction</NavLink> */}
+      </>
+    </>
+  );
+};
+
+export default NavBar;
