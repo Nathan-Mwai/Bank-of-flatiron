f33b0deae9e8e3c53e0722a746c8d62303f88e0a
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-07 10:40:22 +0300

    Removes NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
-import NavBar from './Components/NavBar';
+
 
 function App() {
 
   
   return (
     <div>
-      {/* <NavBar/> */}
+
       <Home />
     </div>
   );
