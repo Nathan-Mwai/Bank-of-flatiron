135f76e26ac3ef835d3ef63f6e943b0bc8f09a41
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-07 08:18:49 +0300

    Finally success fetch

--- bank/src/Components/Home.jsx
+++ bank/src/Components/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AddTrans from './AddTrans'
+import TransDetails from './TransDetails'
 
 const Home = () => {
+    const [transactions, setTransactions] = useState([])
+    
+    useEffect(() => {
+      fetch("http://localhost:3000/transactions")
+      .then(res => res.json())
+      .then(transactions => setTransactions(transactions))
+      .catch(err => console.log(err))
+  }, [])
+
+
+    const disTrans = transactions.map(transaction => <TransDetails key={transaction.id} transaction={transaction} setTransactions={setTransactions}/>) 
   return (
     <div>
+        {disTrans}
         <AddTrans/>
     </div>
   )
---
+++ bank/src/Components/TransDetails.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const TransDetails = ({transaction}) => {
+  return (
+    <div>
+        <table>
+            <caption>{transaction.category}</caption>
+            <thead>
+                <tr>
+                    <td></td>
+                    <th>
+                        <span>Amount</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>
+                        {transaction.date}
+                        <span>
+                            {transaction.description}
+                        </span>
+                    </th>
+                    <td>
+                        {transaction.amount}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TransDetails
