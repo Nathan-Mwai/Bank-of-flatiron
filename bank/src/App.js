3fe84120b4e18ee4f4362184f8631d0cca392090
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-07 09:17:33 +0300

    updates styles

--- bank/src/App.js
+++ bank/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import NavBar from './Components/NavBar';
 
 function App() {
+
+  
   return (
     <div>
+      {/* <NavBar/> */}
       <Home />
     </div>
   );
