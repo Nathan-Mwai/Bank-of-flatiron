0bcf287b6aa31fb580054fbce6c28e4762687077
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-08-08 07:43:27 +0300

    Adds text of search Transaction

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -30,6 +30,7 @@ const Home = () => {
             <h1>FlatIron Bank</h1>
             <p>Welcome to FlatIron Bank, where banking meets excitement! At FlatIron Bank, we’re all about blending tradition with a modern twist to make your financial journey as vibrant and dynamic as you are. Whether you're aiming to grow your savings, dive into smart investments, or enjoy seamless digital banking, we’ve got you covered with a dash of flair and a lot of enthusiasm. With a legacy of trust and a future full of innovation, FlatIron Bank is here to make your financial experience not just secure, but downright exhilarating!</p>
             <AddTrans />
+            <h3>Search Transactions</h3>
             <input
                 type="text"
                 placeholder="Search transactions..."
